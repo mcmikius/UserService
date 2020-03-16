23bca46781d5703ef8a114a42f8a0c674b77606c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-16 22:31:42 +0200

    some

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,14 +16,20 @@ public func configure(_ app: Application) throws {
     app.databases.use(try .mysql(url: url), as: .mysql)
     // uncomment to serve files from /Public folder
     app.middleware.use(CORSMiddleware())
+    app.middleware.use(ErrorMiddleware() { request, error in
+        return Response(status: .internalServerError)
+    })
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     
     try app.jwt.signers.use(jwksJSON: jwksString)
+    
+    guard let sendgridApiKey = Environment.get("SENDGRID_API_KEY") else {
+               fatalError("No value was found at the given public key environment SENDGRID_API_KEY")
+    }
+    let sendgridClient = SendGridClient(client: app.client, apiKey: sendgridApiKey)
 
     app.migrations.add(CreateTodo())
 
     // register routes
-    try routes(app)
+    try routes(app, sendgridClient)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,8 +1,17 @@
 import Fluent
 import Vapor
+import SendGrid
+import SimpleJWTMiddleware
 
-func routes(_ app: Application) throws {
-    root.get([.anything, "name", "health"]) { request in
-        return "Healthy!"
+func routes(_ app: Application, _ sendgridClient: SendGridClient) throws {
+    let root = app.grouped(.anything, "users")
+    let auth = root.grouped(SimpleJWTMiddleware())
+
+    root.get("health") { request in
+        return "All good!"
     }
+    
+//    try root.register(collection: AddressController())
+//    try root.register(collection: AuthController(sendgridClient))
+//    try auth.register(collection: UserController())
 }
